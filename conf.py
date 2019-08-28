7da4fbe68f2adb78b3f9e332201bbb5e10547a47
robkdy <rob2tran@gmail.com>
2019-08-27 22:15:44 -0500

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Robin Tran"  # (translatable)
-BLOG_TITLE = "Robin Tran"  # (translatable)
+BLOG_TITLE = "MORE DATA"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://robkdy.github.io/"
